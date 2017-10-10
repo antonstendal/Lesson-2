bb9ca0a46f265effb02faa662673a32be00902b1
Stendal <antonstendal19@mail.com>
2017-10-10 22:31:23 +0300

    First commit

---
+++ script2.js
@@ -0,0 +1,25 @@
+//ƒан массив 1,2,3, строка 5,6 найти строку в массиве и вывести на экран
+var  searchM = [1,2,3,'array',5,6]; 
+	for (var i = 0; i < searchM.length; i++){
+		if (typeof(searchM[i]) == 'string')
+			break;
+		console.log(searchM[i]);
+	
+}
+
+//Ќаписать плагин, который находит число в массиве
+(function () {
+    var numberN = [2,6,1,4,3,9,8,7]
+	for (i = 0; i < numberN.length; i++){
+	if (numberN[i] == 8){
+		console.log(numberN[i])
+		}
+	}
+ }());
+ 
+ //ѕлагин, который упор€дочивает массив в обратном пор€дке
+ var reverseOne = [1,2,3,4,5,6];
+ console.log(reverseOne.reverse());
+
+
+
